28e8a73445695d10d38287a85c96bc2b5a38a410
Luigi160397 <luigi.difraia.97@gmail.com>
2023-05-04 17:21:03 +0200

    test

--- src/esercizio4/Main4.java
+++ src/esercizio4/Main4.java
@@ -12,7 +12,7 @@ public class Main4 {
 	
 	public static void countDown() {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Inserisci un numero da far partire il count-down:");
+		System.out.println("Inserisci un numero per far partire il count-down:");
         int input = scanner.nextInt();
         for (int i = input; i >= 0; i--) {
 			System.out.println(i);
