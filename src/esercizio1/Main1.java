3175b7e374ef7f63eb6207ca35bbd10c5a12e115
Luigi160397 <luigi.difraia.97@gmail.com>
2023-05-04 17:14:49 +0200

    anno bisestile fixed

--- src/esercizio1/Main1.java
+++ src/esercizio1/Main1.java
@@ -14,16 +14,18 @@ public class Main1 {
 		return word.length() % 2 == 0 ? true : false;
 	}
 	
-public static boolean annoBisestile(int anno) {
+
 		
-	if (anno % 4 == 0 ) {
-		return true;
-	}else if(anno % 100 == 0 && anno % 400 == 0) {
-		return true;
-	} else {
-		return false;
-	}
+	public static boolean annoBisestile(int anno) {
+        if (anno % 4 == 0 && anno % 100 != 0) {
+            return true;
+        } else if (anno % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 	
-}
+
 
 }
