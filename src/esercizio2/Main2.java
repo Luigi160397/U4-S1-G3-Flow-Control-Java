c71aa499383b29649cf581a8c81c8ee0f802be01
Luigi160397 <luigi.difraia.97@gmail.com>
2023-05-04 15:19:24 +0200

    esercizio3

--- src/esercizio2/Main2.java
+++ src/esercizio2/Main2.java
@@ -3,8 +3,29 @@ package esercizio2;
 public class Main2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		stampaInLettere(4);
 
+	}
+	
+	public static void stampaInLettere (int num) {
+		if (num >= 0 && num <= 3) {
+	        switch (num) {
+	            case 0:
+	                System.out.println("zero");
+	                break;
+	            case 1:
+	                System.out.println("uno");
+	                break;
+	            case 2:
+	                System.out.println("due");
+	                break;
+	            case 3:
+	                System.out.println("tre");
+	                break;
+	        }
+	    } else {
+	        System.out.println("Il numero deve essere compreso tra 0 e 3");
+	    }
 	}
 
 }
